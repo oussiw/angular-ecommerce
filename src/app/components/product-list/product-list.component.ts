cc5ae983420da754ba273477b1b6e49e73a382ec
oussiw <ous.siw2@gmail.com>
2020-10-07 00:30:17 +0100

    commit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -29,8 +30,14 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId){
       // get the id end convert it to a number
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
-    else { this.currentCategoryId = 1; }
+    else {
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
+    }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
