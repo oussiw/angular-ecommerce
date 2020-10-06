a6ad2f3e7ad52685f94c2bd33e433bec62e41d6e
oussiw <ous.siw2@gmail.com>
2020-10-06 21:26:59 +0100

    commit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import {Product} from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
